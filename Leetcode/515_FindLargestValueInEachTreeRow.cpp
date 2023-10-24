0c7dcf71c5f476b9c62656499a368163fa9b01e1
Aryan Gupta <72204017+aryangupta02092002@users.noreply.github.com>
2023-10-24 13:30:41 +0530

    Oct 24 - BFS | Binary Tree

---
+++ Leetcode/515_FindLargestValueInEachTreeRow.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        vector<int> ans;
+        if(root == NULL){
+            return ans;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int sz = q.size();
+            int mx = INT_MIN;
+            if(sz == 0){
+                break;
+            }
+
+            while(sz--){
+                TreeNode *curr = q.front();
+                q.pop();
+
+                mx = max(mx, curr->val);
+
+                if(curr->left){
+                    q.push(curr->left);
+                }
+                if(curr->right){
+                    q.push(curr->right);
+                }
+            }
+            ans.push_back(mx);
+        }
+        return ans;
+    }
+};
